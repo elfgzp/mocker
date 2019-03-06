f875bbb58e177a7f91c2e3dca5278945e923d47a
elfgzp <741424975@qq.com>
2019-03-06 17:14:53 +0800

    [ADD] add cgroups code

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
+	"strconv"
 	"syscall"
 )
 
+//挂载了 memory subsystem 的 hierarchy 的根目录位置
+const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+
 func main() {
-	cmd := exec.Command("sh")
+	if os.Args[0] == "/proc/self/exe" {
+		// 容器进程
+		fmt.Printf("current pid %d\n", syscall.Getpid())
+		// 需要安装 stress
+		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	cmd := exec.Command("/proc/self/exe")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
 
 	cmd.Stdin = os.Stdin
@@ -18,6 +40,18 @@ func main() {
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+		fmt.Println("ERROR", err)
+		os.Exit(1)
+	} else {
+		//得到 fork 出来进程映射在外部命名空间的 pid
+		fmt.Printf("%v", cmd.Process.Pid)
+
+		//在系统默认创建挂载了 memory subsystem 的 Hierarchy 上创建 cgroup
+		os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 0755)
+		//将容器进程加入到这个 cgroup 中
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"), []byte(strconv.Itoa(cmd.Process.Pid)), 0644)
+		//限制 cgroup 进程使用
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"), []byte("100m"), 0644)
 	}
+	cmd.Process.Wait()
 }
